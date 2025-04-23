57c308ba7dcc0f408002c050ca980a30ca94196e
Jeff McKean <jmckean44@gmail.com>
2025-04-23 12:42:43 -0400

    Workflow & Vite config updates

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	base: '/Flixx/', // Replace <repository-name> with your GitHub repo name
+	base: '/flixx/', // Replace <repository-name> with your GitHub repo name
 });

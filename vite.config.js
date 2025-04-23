25e1b0f5f17076a6227649764d6be3feb88afb3f
Jeff McKean <jmckean44@gmail.com>
2025-04-23 12:17:26 -0400

    Final Project install Vite & GitHub Pages

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+// filepath: /Users/design3/Jeff/www/Projects/Flixx/vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	base: '/Flixx/', // Replace <repository-name> with your GitHub repo name
+});
